e9fd9e0ad1639fcfbb239f31421d3dcbd1ab650e
OrnellaRobs <orazafin@student.42.fr>
2017-04-07 15:53:23 +0200

    Fillit

--- src/main.c
+++ src/main.c
@@ -6,31 +6,30 @@
 /*   By: orazafin <orazafin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 00:19:31 by orazafin          #+#    #+#             */
-/*   Updated: 2017/02/20 19:11:49 by orazafin         ###   ########.fr       */
+/*   Updated: 2017/04/07 15:51:58 by orazafin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int				open_tetriminos_file(char const *file)
+static int				open_tetriminos_file(char const *file, int argc)
 {
 	int fd;
 
+	if (argc != 2)
+	{
+		ft_putstr("usage: ./fillit.h file.txt\n");
+		return (-1);
+	}
 	fd = open(file, O_RDONLY);
 	return (fd);
 }
 
-static t_double_list	*read_tetriminos_file(int fd, t_double_list *li,
-int argc)
+static t_double_list	*read_tetriminos_file(int fd, t_double_list *li)
 {
 	char	*buff;
 	int		read_octet;
 
-	if (argc > 2)
-	{
-		ft_putstr("usage: ./fillit.h file.txt\n");
-		return (NULL);
-	}
 	if (!(buff = (char *)malloc(sizeof(char) * BUFF_SIZE + 1)))
 		return (NULL);
 	if (((read_octet = read(fd, buff, BUFF_SIZE))) == (-1))
@@ -78,9 +77,9 @@ int						main(int argc, char const *argv[])
 
 	li = NULL;
 	array = create_and_initialize_final_array_with_dots(2);
-	if ((fd = open_tetriminos_file(argv[1])) == (-1))
+	if ((fd = open_tetriminos_file(argv[1], argc)) == (-1))
 		return (-1);
-	li = read_tetriminos_file(fd, li, argc);
+	li = read_tetriminos_file(fd, li);
 	if (li == NULL)
 		return (0);
 	tmp = li->begin;
